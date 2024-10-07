0a980879c2de501455ddc452ba9af297e8d23f0c
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-07 15:28:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="in">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+  <h1>Strong Password Generator</h1>
+  <h2>Genera una passsword sicura</h2>
+</body>
+
+</html>
